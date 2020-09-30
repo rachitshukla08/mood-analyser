ec2ab15c7cd64c7f6624c8c952c9b33b7293b9d9
unknown <rachitshukla08@gmail.com>
2020-09-30 15:51:21 +0530

    Added: MoodAnalyser class to analyse happy or sad mood

--- main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
+++ main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -13,7 +13,10 @@ public class MoodAnalyser {
 	 * @param args
 	 */
 	public String analyseMood(String message) {
+		if(message.contains("sad"))
 			return "SAD";
+		else 
+			return "HAPPY";
 	}
 	public static void main(String[] args) {
 		MoodAnalyser moodAnalyser = new MoodAnalyser();
